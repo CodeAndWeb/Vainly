0e08e970a06fd0fbc5402f113651c5961ffd3346
Andreas Loew <andreas@codeandweb.com>
2018-07-26 20:38:58 +0200

    Fixed sprite assignment

--- Level.js
+++ Level.js
@@ -51,7 +51,7 @@ class Level extends Phaser.Scene {
         this.addSprite(400, -8271, 'sprites','ob6',{shape: shapes.ob6});
 
         //var aL = this.matter.world.fromPath('2 2  2 6  4 8  14 6  6 2   60 84  56 83  51 88  60 88   79 55  82 55  88 44  90  40  87 33  83 29  75 27  78 52   48 17  44 17  41 20  46 24  49 22   56 83  54 79  50 77  27 71  29, "y":84  34 87  43 88  51 88   53 21  51 25  69 25  59 21   31 87  34 87  29 84   46 24  41 20  38 20  30 44  44 31   85 61  88 60  88 44  82 55   90 35  87 33  90 40   35 90  43 88  34 87   17 49  19 50  24 45  20 38  17 44   24 45  30 44  38 20  38 18  20 38   87 31  83 29  87 33   51 25  44 31  30 44  72 28  69 25   16 37  20 38  38 18  14 6  12 33   29 10  14 6  38 18   78 52  75 27  73 51   4 8  12 33  14 6   73 51  75 27  72 28  30 44  30 46  59 68  64 67   50 77  50 73  27 71   50 73  59 68  30 46  21 53  20 56  23 66  27 71');
-        player = this.addSprite(400, 1000, 'sprites','arrowStraight',{shape: shapes.arrowStraight});
+        player = this.addSprite(400, 1000, 'sprites','arrowStraight.png',{shape: shapes.arrowStraight});
         player.setVelocityY(-13);
         player.setScale(0.7, 0.7);
         player.setFriction(0,0,0);
@@ -61,14 +61,14 @@ class Level extends Phaser.Scene {
         //obstacles.setPosition(400 + obstacles.centerOfMass.x, -4000 + obstacles.centerOfMass.y);
         //player.setCollideWorldBounds(true);
         this.input.on('pointerdown', function (pointer) {
-            player.setTexture('sprites','arrowLeft',{shape: shapes.arrowLeft});
+            player.setTexture('sprites','arrowLeft.png',{shape: shapes.arrowLeft});
             player.setVelocityX(-13);
             player.boundsMargin = 0;
             player.setInteractive();
         });
 
         this.input.on('pointerup', function () {
-            player.setTexture('sprites','arrowRight',{shape: shapes.arrowRight});
+            player.setTexture('sprites','arrowRight.png',{shape: shapes.arrowRight});
             player.setVelocityX(13);
             player.boundsMargin = 0;
             player.setInteractive();
